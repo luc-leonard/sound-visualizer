143cbe87754d093865d6a6c530e1a6f3df2a7ce1
Luc Leonard <github@luc-leonard.fr>
2020-11-20 22:28:21 +0100

    feat(gunicorn): added a config file for gunicorn

---
+++ gunicorn_config.py
@@ -0,0 +1,7 @@
+workers = 3              # number of workers Gunicorn will spawn
+
+bind = '0.0.0.0:8000'  # this is where you declare on which address your
+                         # gunicorn app is running.
+                         # Basically where Nginx will forward the request to
+
+daemon = False          # this is only to tell gunicorn to deamonize the server process
