4bf92975da95a69bbb729264c7fb1d98a52d5bcb
Luc Leonard <github@luc-leonard.fr>
2020-11-21 15:22:08 +0100

    feat(mp3): converted mp3 are now in /tmp/

--- sound_visualizer/app/sound/converter/mp3.py
+++ sound_visualizer/app/sound/converter/mp3.py
@@ -5,6 +5,6 @@ import ffmpeg
 
 class Mp3Converter:
     def convert(self, filename: str) -> str:
-        the_filename = str(uuid.uuid4()) + '.wav'
+        the_filename = f'/tmp/{str(uuid.uuid4())}.wav'
         ffmpeg.input(filename).output(the_filename, loglevel='info').run()
         return the_filename
