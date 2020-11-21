05f6dba6f1aa9af1ae3c476f6562ccebfd3a88db
Luc Leonard <github@luc-leonard.fr>
2020-11-21 20:28:45 +0100

    feat(youtubeDownloader): download now returns the path to the mp3 file

--- sound_visualizer/app/input/downloader/youtube.py
+++ sound_visualizer/app/input/downloader/youtube.py
@@ -1,7 +1,31 @@
+import logging
+
 import youtube_dl
 
 
+class YoutubeHook:
+    def __call__(self, *args, **kwargs):
+        data = args[0]
+        if data['status'] == 'finished':
+            self.final_status = data
+
+
 class YoutubeDownloader:
-    def download(self):
-        with youtube_dl.YoutubeDL({'prefer_ffmpeg': True}) as ydl:
-            return ydl.download(['https://www.youtube.com/watch?v=BaW_jenozKc'])
+    def download(self, url):
+        hook = YoutubeHook()
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'outtmpl': '/tmp/%(title)s-%(id)s.%(ext)s',
+            'postprocessors': [
+                {
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'mp3',
+                    'preferredquality': '192',
+                }
+            ],
+            'logger': logging.getLogger(__name__),
+            'progress_hooks': [hook],
+        }
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+            return hook.final_status['filename'][0:-4] + '.mp3'
