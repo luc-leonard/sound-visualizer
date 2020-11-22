6f42d19c7d800a489db9550b50868d740ec7cb1b
luc-leonard <github@luc-leonard.fr>
2020-11-22 16:13:22 +0100

    feat(fft): the computation of fft should now take A LOT less memory (#49)

--- sound_visualizer/utils/size.py
+++ sound_visualizer/utils/size.py
@@ -1,5 +1,7 @@
 import math
 
+import numpy as np
+
 
 def convert_size(size_bytes):
     if size_bytes == 0:
@@ -9,3 +11,9 @@ def convert_size(size_bytes):
     p = math.pow(1024, i)
     s = round(size_bytes / p, 2)
     return "%s %s" % (s, size_name[i])
+
+
+def np_get_real_size(obj: np.ndarray) -> int:
+    if obj.base is not None:
+        return np_get_real_size(obj.base)
+    return obj.nbytes
