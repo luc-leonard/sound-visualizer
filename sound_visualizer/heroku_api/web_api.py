6b07d4670ebd27af8645590ebed30f9eb1ea9349
Luc Leonard <github@luc-leonard.fr>
2020-11-22 19:16:03 +0100

    maybe it works ?...

--- sound_visualizer/heroku_api/web_api.py
+++ sound_visualizer/heroku_api/web_api.py
@@ -1,9 +1,13 @@
+import io
+import json
 import logging
-import os
 import random
+import threading
 
-from flask import Flask
+from flask import Flask, redirect, request, send_file
 from google.cloud import pubsub_v1
+from google.cloud.storage.client import Client as CloudStorageClient
+from werkzeug.utils import secure_filename
 
 from sound_visualizer.utils.logger import init_logger
 
@@ -32,17 +36,47 @@ def get_form():
        '''
 
 
+publisher = pubsub_v1.PublisherClient()
+
+storage_client = CloudStorageClient()
+bucket = storage_client.bucket('spectrogram-images')
+topic_path = publisher.topic_path('luc-leonard-sound-visualizer', 'my-topic')
+
+
+def upload_file(filename, path):
+    blob = bucket.blob(filename)
+    blob.upload_from_filename(path, timeout=600)
+    logger.info(f'{filename} uploaded')
+
+
+@app.route('/result/<result_id>', methods=['GET'])
+def get_image(result_id):
+    logger.info('GETTING IMAGE')
+    data = io.BytesIO()
+    blob = bucket.blob(result_id + '.png')
+    blob.download_to_file(data)
+    data.seek(0)
+    return send_file(data, attachment_filename='_result.png', cache_timeout=0)
+
+
 @app.route('/', methods=['POST'])
 def post_image():
-    logger.warning(os.environ)
-    publisher = pubsub_v1.PublisherClient()
-    topic_path = publisher.topic_path('luc-leonard-sound-visualizer', 'my-topic')
-    print(topic_path)
-    data = "Message number {}".format(random.randint(0, 100))
-    # Data must be a bytestring
-    future = publisher.publish(topic_path, data.encode("utf-8"))
+    data = request.form.to_dict()
+    data['result_id'] = 'result_' + str(random.randint(0, 100))
 
-    return future.result()
+    if len(request.form['youtube_url']) == 0:
+        # upload to object storage
+        sound_file = request.files['sound_file']
+        filename = '/tmp/' + secure_filename(sound_file.filename)
+        sound_file.save(filename)
+        data['filename'] = sound_file.filename
+
+        def _thread(data_cpy):
+            upload_file(sound_file.filename, filename)
+            publisher.publish(topic_path, json.dumps(data).encode("utf-8"))
+
+        threading.Thread(target=_thread, args=(data,)).start()
+        return redirect('/result/' + data['result_id'])
 
 
 if __name__ == '__main__':
