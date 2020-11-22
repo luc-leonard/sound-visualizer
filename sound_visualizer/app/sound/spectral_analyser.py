5eddc7183f3873abcafa7fed6c510d3a03fa6ad6
Luc Leonard <github@luc-leonard.fr>
2020-11-22 16:06:50 +0100

    feat(fft): the computation of fft should now take A LOT less memory

--- sound_visualizer/app/sound/spectral_analyser.py
+++ sound_visualizer/app/sound/spectral_analyser.py
@@ -3,8 +3,9 @@ import logging
 import numpy as np
 from pydantic import BaseModel, Field
 
-from sound_visualizer import utils
 from sound_visualizer.app.input import SoundReader
+from sound_visualizer.utils import convert_size
+from sound_visualizer.utils.size import np_get_real_size
 
 logger = logging.getLogger(__name__)
 
@@ -72,16 +73,32 @@ def get_spectogram_data(data: np.ndarray, frame_size: int, overlap_factor: float
     samples = np.append(np.zeros(int(np.floor(frame_size / 2.0))), data)
     cols = np.ceil((len(samples) - frame_size) / float(hopSize)) + 1
     samples = np.append(samples, np.zeros(frame_size))
+
     # first, we create overlapping windows (in the sql sense) with as_strided.
-    # then we copy them so each window will be modifiable
+    # since those are views on the original array, it costs almost nothing in memory
     frames = stride_tricks.as_strided(
         samples,
         shape=(int(cols), frame_size),
         strides=(samples.strides[0] * hopSize, samples.strides[0]),
-    ).copy()
-    logger.info(f'total frames size = {utils.size.convert_size(frames.nbytes)}')
-    # we apply a 'window' (in the mathematical sense) function. its purpose is to make sure our lines are smooth
-    frames *= np.hamming(frame_size)
+    )
+    # todo: either make this configurable, or find an heuristic to get the optimal value
+    nb_chunks = 100
+    logger.info(
+        f"the fft will be computed on {nb_chunks} chunks. size in memory {convert_size(np_get_real_size(frames))}"
+    )
+    # we split this array in chunks
+    chunked_frames = np.array_split(frames, nb_chunks)
+    window_fn = np.hamming(frame_size)
 
-    full_fft_data = 10 * np.log(10) * np.abs(np.fft.rfft(frames))
+    full_fft_data = None
+    for frame in chunked_frames:
+        # we apply a 'window' (in the mathematical sense) function. its purpose is to make sure our lines are smooth
+        # since it alter a frame, and frames are view that overlap, we need to copy data first
+        local_frame = frame.copy()
+        local_frame *= window_fn
+        frame_fft_data = 10 * np.log(10) * np.abs(np.fft.rfft(local_frame))
+        if full_fft_data is not None:
+            full_fft_data = np.vstack([full_fft_data, frame_fft_data])
+        else:
+            full_fft_data = frame_fft_data
     return full_fft_data
