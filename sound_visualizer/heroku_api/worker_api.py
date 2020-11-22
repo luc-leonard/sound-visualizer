d6a41700495ac7784454e1cbd937e0fb021d1f5c
Luc Leonard <github@luc-leonard.fr>
2020-11-22 19:36:56 +0100

    it should work for youtube as well :D

--- sound_visualizer/heroku_api/worker_api.py
+++ sound_visualizer/heroku_api/worker_api.py
@@ -8,6 +8,8 @@ from google.cloud import pubsub_v1
 from google.cloud.storage.client import Client as CloudStorageClient
 
 from sound_visualizer.app.input import SoundReader
+from sound_visualizer.app.input.converter import Mp3Converter
+from sound_visualizer.app.input.downloader.youtube import YoutubeDownloader
 from sound_visualizer.app.output.grey_scale_image import GreyScaleImageGenerator
 from sound_visualizer.app.sound import SpectralAnalyzer
 from sound_visualizer.utils.logger import init_logger
@@ -22,24 +24,30 @@ def download_file(bucket_filename, local_filename):
 
 
 def callback(message):
-    data = json.loads(message.data)
-    logger.info(data)
-    filename = '/tmp/' + str(random.randint(0, 255))
-
-    bucket.blob(data['filename']).download_to_filename(filename)
-
-    del data['filename']
-    sound_reader = SoundReader(filename=filename, **data)
-    spectral_analyser = SpectralAnalyzer(frame_size=4096, overlap_factor=0.6)
-    spectral_analysis = spectral_analyser.get_spectrogram_data(sound_reader)
-    image = GreyScaleImageGenerator(border_width=10, border_color='red').create_image(
-        spectral_analysis.fft_data
-    )
-    with io.BytesIO() as bytes:
-        image.save(bytes, format='png')
-        bytes.seek(0)
-        bucket.blob(data['result_id'] + '.png').upload_from_file(bytes)
-    message.ack()
+    try:
+        data = json.loads(message.data)
+        logger.info(data)
+        if len(data['youtube_url']) == 0:
+            filename = '/tmp/' + str(random.randint(0, 255))
+            bucket.blob(data['filename']).download_to_filename(filename)
+            del data['filename']
+        else:
+            filename = YoutubeDownloader().download(data['youtube_url'])
+            filename = Mp3Converter(filename=filename, **data).convert()
+        sound_reader = SoundReader(filename=filename, **data)
+        spectral_analyser = SpectralAnalyzer(frame_size=4096, overlap_factor=0.6)
+        spectral_analysis = spectral_analyser.get_spectrogram_data(sound_reader)
+        image = GreyScaleImageGenerator(border_width=10, border_color='red').create_image(
+            spectral_analysis.fft_data
+        )
+        with io.BytesIO() as bytes:
+            image.save(bytes, format='png')
+            bytes.seek(0)
+            bucket.blob(data['result_id'] + '.png').upload_from_file(bytes)
+    except Exception as e:
+        logger.error('error handling message', e)
+    finally:
+        message.ack()
 
 
 if __name__ == '__main__':
