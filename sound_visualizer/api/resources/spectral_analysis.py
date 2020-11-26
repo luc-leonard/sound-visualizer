2c8db231803ec1e185ee133447f57ec82058b81f
Luc Leonard <github@luc-leonard.fr>
2020-11-26 22:35:58 +0100

    feature(api): it is now possible to get list of all analysis as json

--- sound_visualizer/api/resources/spectral_analysis.py
+++ sound_visualizer/api/resources/spectral_analysis.py
@@ -1,6 +1,14 @@
 from flask_restful import Resource
 
+from sound_visualizer.models.spectral_analysis_request import SpectralAnalysisRequestORM
 
-class SpectralAnalysisResource(Resource):
-    def get(self) -> list:
-        ...
+
+def SpectralAnalysisResource(orm: SpectralAnalysisRequestORM):
+    class InnerClass(Resource):
+        def __init__(self):
+            self.orm = orm
+
+        def get(self) -> list:
+            return [o.dict() for o in self.orm.load_all_requests()]
+
+    return InnerClass
