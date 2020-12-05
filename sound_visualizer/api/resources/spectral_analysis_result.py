7a6c0a15a2b6d2823efc3333fc515ab39384905f
Luc Leonard <github@luc-leonard.fr>
2020-12-05 17:31:44 +0100

    feat(cached storage): this should simplify the cache usage

--- sound_visualizer/api/resources/spectral_analysis_result.py
+++ sound_visualizer/api/resources/spectral_analysis_result.py
@@ -1,10 +1,9 @@
-import io
 import logging
 
 from flask import send_file
 from flask_restful import Resource
 
-from sound_visualizer.app.cache import Cache
+from sound_visualizer.app.cache import Cache, CachedStorage
 from sound_visualizer.app.storage.storage import Storage
 from sound_visualizer.models.spectral_analysis_request import SpectralAnalysisFlowORM
 
@@ -12,20 +11,14 @@ logger = logging.getLogger(__name__)
 
 
 def SpectralAnalysisResultResource(orm: SpectralAnalysisFlowORM, storage: Storage, cache: Cache):
+    cached_storage = CachedStorage(cache, storage)
+
     class SpectralAnalysisResultImpl(Resource):
         def get(self, analysis_id: str):
             flow = orm.load_request_by_id(analysis_id)
             if flow.status != 'finished':
                 return flow.dict()
-            if cache.is_data_in_cache(analysis_id):
-                data = cache.get_data_in_cache(analysis_id)
-            else:
-                logger.info('GETTING IMAGE FROM BUCKET')
-                data = io.BytesIO()
-                storage.download_to(analysis_id + '.png', data)
-                data.seek(0)
-                cache.put_data_in_cache(analysis_id, data)
-                data.seek(0)
+            data = cached_storage.get(analysis_id)
             # the cache duration can be VERY high, results are immutables.
             # But since we are in a WIP, let's put it low, so we can fix things :P
             return send_file(data, attachment_filename='_result.png', cache_timeout=600)
