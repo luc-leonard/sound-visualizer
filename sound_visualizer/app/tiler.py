d19787f7df053f94156bb013d68c2b03a5b2a729
Luc Leonard <github@luc-leonard.fr>
2020-12-06 16:30:22 +0100

    feat(tiling): HorizontalTiler should be working

--- sound_visualizer/app/tiler.py
+++ sound_visualizer/app/tiler.py
@@ -1,30 +1,35 @@
 import io
 import logging
 import typing
+from pathlib import Path
 
-from sound_visualizer.app.cache import Cache
+import PIL.Image
+
+from sound_visualizer.app.cache import Cache, CachedStorage
 from sound_visualizer.app.storage.storage import Storage
 
 logger = logging.getLogger(__name__)
 
 
 class HorizontalTiler:
-    def __init__(self, cache: Cache, storage: Storage):
-        self.cache = cache
-        self.storage = storage
+    def __init__(self, cache: Cache, storage: Storage, tile_width: int):
+        self.cache = CachedStorage(cache, storage)
+        self.tile_caches = Cache(cache_folder=Path('/tmp/tiles/'))
+        self.tile_width = tile_width
 
     def get_tile(self, image_id: str, tile_idx: int) -> typing.BinaryIO:
-        self._load_image_if_necessary(image_id)
-        return io.BytesIO()
+        cached_tile_key = f'{image_id}_{tile_idx}'
+        if self.tile_caches.is_data_in_cache(cached_tile_key):
+            return self.tile_caches.get_data_in_cache(cached_tile_key)
+        data = self.cache.get(image_id)
+        image: PIL.Image.Image = PIL.Image.open(data, formats=('PNG',))
+        left_idx = self.tile_width * tile_idx
+        right_idx = left_idx + self.tile_width
 
-    def _load_image_if_necessary(self, image_id) -> None:
-        logger.info(vars())
-        if self.cache.is_data_in_cache(image_id):
-            data = self.cache.get_data_in_cache(image_id)
-        else:
-            logger.info('GETTING IMAGE FROM BUCKET')
-            data = io.BytesIO()
-            self.storage.download_to(image_id + '.png', data)
-            data.seek(0)
-            self.cache.put_data_in_cache(image_id, data)
-            data.seek(0)
+        cropped_image = image.crop((left_idx, 0, right_idx, image.height))
+        cropped_data = io.BytesIO()
+        cropped_image.save(cropped_data, format='PNG')
+        cropped_data.seek(0)
+        self.tile_caches.put_data_in_cache(cached_tile_key, cropped_data)
+        cropped_data.seek(0)
+        return cropped_data
