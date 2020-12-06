a62987c679be1a4789c058b5a9b67bda69ca3705
Luc Leonard <github@luc-leonard.fr>
2020-12-06 16:30:22 +0100

    feat(tiling): HorizontalTiler should be working

--- sound_visualizer/api/resources/tiles_resource.py
+++ sound_visualizer/api/resources/tiles_resource.py
@@ -1,4 +1,3 @@
-import io
 import logging
 
 from flask import send_file
@@ -6,23 +5,18 @@ from flask_restful import Resource
 
 from sound_visualizer.app.cache import Cache
 from sound_visualizer.app.storage.storage import Storage
+from sound_visualizer.app.tiler import HorizontalTiler
 
 logger = logging.getLogger(__name__)
 
 
 def TilesResources(storage: Storage, cache: Cache):
+    tiler = HorizontalTiler(cache, storage, 500)
+
     class TilesResourcesImpl(Resource):
         def get(self, result_id: str, x: int):
             logger.info(vars())
-            if cache.is_data_in_cache(result_id):
-                data = cache.get_data_in_cache(result_id)
-            else:
-                logger.info('GETTING IMAGE FROM BUCKET')
-                data = io.BytesIO()
-                storage.download_to(result_id + '.png', data)
-                data.seek(0)
-                cache.put_data_in_cache(result_id, data)
-                data.seek(0)
+            data = tiler.get_tile(result_id, x)
             return send_file(data, attachment_filename='_result.png', cache_timeout=10)
 
     return TilesResourcesImpl
