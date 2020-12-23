c2cdcf3f938ddbb32fecd0d1c3250e551396ba41
Luc Leonard <github@luc-leonard.fr>
2020-12-23 14:10:47 +0100

    disable youtube dl test, this is dangerous when on CI

--- tests/app/downloader/test_youtube.py
+++ tests/app/downloader/test_youtube.py
@@ -1,6 +1,10 @@
+import pytest
+
 from sound_visualizer.app.downloader import YoutubeDownloader
 
 
+# This is skipped so we do not get banned from youtube :)
+@pytest.mark.skip()
 def test_youtube():
     downloaded_file = YoutubeDownloader().download('https://www.youtube.com/watch?v=tkG4EMP2tC8')
     assert downloaded_file is not None
